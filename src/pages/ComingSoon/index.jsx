707fd00dfb15429dfac99002c4198c4b48d9fef9
daniekeys <starlordflash@gmail.com>
2022-09-25 02:31:36 +0100

    updated background

--- src/pages/ComingSoon/index.jsx
+++ src/pages/ComingSoon/index.jsx
@@ -4,6 +4,8 @@ import {FaCopyright} from "react-icons/fa"
 import ResponseModal from './Modal';
 import MobileComingSoon from './Mobile';
 import { useCountdown } from './useCountdown';
+import NormalLayout from '../../layouts/NormalLayout';
+import { AiOutlineAudio, AiOutlineAudioMuted } from "react-icons/ai";
 import logo from "../../assets/sportrex-logo.png";
 import ComingImg from "../../assets/coming-soon/coming-soon-ocu.svg";
 import telegram from "../../assets/icons/telegram.png";
@@ -80,61 +82,75 @@ const ComingSoon = () => {
 
   const [days, hours, minutes, seconds] = useCountdown("2022-10-10");
   const [open, setOpen] = useState();
+  
 
   const handleClose = () => {
     setOpen(false);
   }
   return (
     <>
-      <div className="w-full min-h-screen bg-blue-commingSoon relative overflow-x-hidden  md:flex justify-center items-center hidden ">
+      <div className="w-full min-h-screen bg-blue-commingSoon relative overflow-x-hidden  md:flex justify-center items-center hidden  ">
         <ResponseModal open={open} handleClose={handleClose} />
         <div className="coming-soon-bg ">
-          <div className="2xl:container 2xl:mx-auto max-w-[1440px] mx-auto flow-hide h-screen">
-            <div className="w-full flex flex-col items-center justify-center">
-              <div className="w-3/12 h-auto mt-20">
-                <img src={logo} alt="logo" className="w-full h-auto" />
-              </div>
-              <div className="in w-full justify-center items-center">
-                <h1 className="sm:text-[36px] md:text-[40px] xl:text-[48px] mx-auto sm:w-10/12 lg:w-7/12 2xl:w-6/12 text-white mt-5 bold text-center ">
-                  Your seamless hallway to a world of possibilities
-                </h1>
-              </div>
-              <h1 className="text-white text-center text-[36px] mt-4 bold">
-                Anticipate
-                <span className="coming-soon-text ml-2">
-                  Sportrex NFT MarketPlace
-                </span>
-              </h1>
-              <div className="flex flex-col">
-                <p className="text-white mt-4 w-8/12 2xl:w-7/12 mx-auto text-center text-[24px] regular leading-9 ">
-                  Home to virtual assets, digital arts and collectibles. Through
-                  our platform, users will be able to access the most amazing
-                  NFT deal for all
-                </p>
-                <div className="flex justify-center  text-white text-[24px] items-center">
-                  categories of users, with VR/AR experience.
-                  <img src={ComingImg} alt="ocu" className="w-12 ml-2 " />
-                </div>
-              </div>
+          <div className="coming-overlay ">
+            <div className="2xl:container 2xl:mx-auto max-w-[1440px] mx-auto flow-hide h-screen ">
+              <NormalLayout>
+                <div className="w-full flex flex-col  justify-center mt-10">
+                  <div className="flex justify-between w-full items-center">
+                    <div className="w-[181px] h-auto ">
+                      <img src={logo} alt="logo" className="w-full h-auto" />
+                    </div>
+                    <div className="flex space-x-8 items-center">
+                      <a
+                        href="https://media.publit.io/file/SPORTREX-WHITE-PAPER.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white regular cursor-pointer"
+                      >
+                        White paper
+                      </a>
+                      <span className="grad-bg w-10 h-10 grid place-items-center rounded-full text-white text-2xl">
+                        <AiOutlineAudio />
+                      </span>
+                    </div>
+                  </div>
 
-              <div className="mt-10 w-full mb-20">
-                <div className="2xl:w-5/12  w-6/12 mx-auto flex items-center bg-[#152139] h-16 rounded-[20px] ">
-                  <input
-                    type="text"
-                    className="w-6/12 mx-auto pl-8 pr-10 py-1 bg-transparent placeholder:text-grey-300 outline-none border-none regular text-white"
-                    placeholder=" Enter your email "
-                  />
-                  <p
-                    className="coming-soon-btn w-3/12 h-full rounded-[20px] cursor-pointer text-white bold text-[20px] text-center flex justify-center items-center"
-                    onClick={() => setOpen(true)}
-                  >
-                    Notify me
-                  </p>
-                </div>
-                <p className="text-center mt-2 text-base text-white regular">
-                  **Notify me when website launch**
-                </p>
-                {/* <div className="time flex flex-col items-center mt-10">
+                  <h1 className="text-white text-start text-[36px] mt-20 bold w-5/12">
+                    Anticipate
+                    <span className="coming-soon-text ml-2">
+                      Sportrex NFT MarketPlace
+                    </span>
+                  </h1>
+                  <div className="flex flex-col">
+                    <p className="text-white mt-4 w-7/12 2xl:w-5/12  text-start text-[24px] regular leading-9 ">
+                      Home to virtual assets, digital arts and collectibles.
+                      Through our platform, users will be able to access the
+                      most amazing NFT deal for all
+                    </p>
+                    <div className="flex justify-start  text-white text-[24px] items-center">
+                      categories of users, with VR/AR experience.
+                      <img src={ComingImg} alt="ocu" className="w-12 ml-2 " />
+                    </div>
+                  </div>
+
+                  <div className="mt-10 w-full mb-20">
+                    <div className="2xl:w-5/12  w-6/12  flex items-center bg-[#152139] h-16 rounded-[20px] ">
+                      <input
+                        type="text"
+                        className="w-6/12 mx-auto pl-6 py-1 bg-transparent placeholder:text-grey-300 outline-none border-none regular text-white"
+                        placeholder=" Enter your email or Ens address "
+                      />
+                      <p
+                        className="coming-soon-btn w-3/12 h-full rounded-[20px] cursor-wait text-white bold text-[20px] text-center flex justify-center items-center"
+                        onClick={() => setOpen(true)}
+                      >
+                        Notify me
+                      </p>
+                    </div>
+                    <p className=" mt-2 text-base text-white regular">
+                      **Notify me when website launch**
+                    </p>
+                    {/* <div className="time flex flex-col items-center mt-10">
                   <h1 className="text-white text-[28px] bold">
                     Time remaining
                   </h1>
@@ -179,38 +195,40 @@ const ComingSoon = () => {
                     </div>
                   </div>
                 </div> */}
-                <div className="flex flex-col items-center mt-16">
-                  <h1 className="text-white text-[28px] text-center bold">
-                    Join the Sportrex community
-                  </h1>
-                  <div className="flex space-x-2 mt-4">
-                    {linksArrayA.map((item, index) => {
-                      return (
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          key={index}
-                          rel="noopener noreferrer"
-                        >
-                          <abbr title={`${item.name}`}>
-                            <img
-                              src={item.icon}
-                              alt="icons"
-                              className="w-12 h-auto"
-                            />
-                          </abbr>
-                        </a>
-                      );
-                    })}
+                    <div className="flex flex-col items-center mt-16">
+                      <h1 className="text-white text-[28px] text-center bold">
+                        Join the Sportrex community
+                      </h1>
+                      <div className="flex space-x-2 mt-4">
+                        {linksArrayA.map((item, index) => {
+                          return (
+                            <a
+                              href={item.link}
+                              target="_blank"
+                              key={index}
+                              rel="noopener noreferrer"
+                            >
+                              <abbr title={`${item.name}`}>
+                                <img
+                                  src={item.icon}
+                                  alt="icons"
+                                  className="w-12 h-auto"
+                                />
+                              </abbr>
+                            </a>
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <div className="mt-16 flex space-x-2 items-center justify-center">
+                      <FaCopyright className="text-white regular text-[28px]" />
+                      <p className="text-white bold text-[18px]">
+                        2022 Sportrex Inc. All right reserved
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <div className="mt-16 flex space-x-2 items-center justify-center">
-                  <FaCopyright className="text-white regular text-[28px]" />
-                  <p className="text-white bold text-[18px]">
-                    2022 Sportrex Inc. All right reserved
-                  </p>
-                </div>
-              </div>
+              </NormalLayout>
             </div>
           </div>
         </div>
